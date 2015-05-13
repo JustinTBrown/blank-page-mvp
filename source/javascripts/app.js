dd56e0b5cc1cf39f20c6f6c61038f504fa30a8b0
JustinBrown <brown.jt@gmail.com>
2015-05-13 02:11:05 -0600

    Added images. More styles.

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 100) {
+  if (y > 200) {
     $('header').fadeIn(1000);
   } else {
     $('header').fadeOut(600);
